78d99177af3b24aa0778afe1c4f8fa3cf6b50c07
denizyoldas <daksu30@gmail.com>
2023-01-08 19:39:29 +0300

    app ctrl add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [EventsModule, UsersModule, AuthModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
