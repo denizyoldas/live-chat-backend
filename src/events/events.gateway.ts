dee2a0835735c4aa35f93eb8870441ca59fcc0bd
denizyoldas <daksu30@gmail.com>
2023-01-09 23:26:55 +0300

    disconnect connect add

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,35 +1,50 @@
 import { ChatService } from './../chat/chat.service';
 import {
   MessageBody,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
+
+const USERS: any[] = [];
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class EventsGateway {
+export class EventsGateway implements OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
   constructor(private chatService: ChatService) {}
 
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'events', data: item })),
-    );
+  handleDisconnect(client: any) {
+    const userId = client.handshake.auth.token;
+    console.log('Disconnected: ', userId);
+
+    const index = USERS.findIndex((user) => user.userId === userId);
+    if (index !== -1) {
+      USERS.splice(index, 1);
+
+      this.server.emit('leave', userId);
+    }
   }
 
-  @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
+  @SubscribeMessage('join')
+  async identity(
+    @MessageBody()
+    data: {
+      userId: string;
+      userName: string;
+      userAvatar: string;
+    },
+  ) {
+    USERS.push(data);
+
+    this.server.emit('join', data);
     return data;
   }
 
