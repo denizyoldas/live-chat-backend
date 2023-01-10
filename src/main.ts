8318325a15b5389d329929c50d2c92438187bf36
denizyoldas <daksu30@gmail.com>
2023-01-10 21:35:03 +0300

    sawgger and prisma add

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './shared/services/prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // cors config
   app.enableCors();
+
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Live Chat App')
+    .setDescription('Live Chat App some api')
+    .setVersion('0.2')
+    .addTag('live-chat-app')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // prisma config
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(2000);
 }
 bootstrap();
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports: [SharedModule],
 })
 export class UsersModule {}
